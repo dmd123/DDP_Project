cf9b146984455b3f355c2479e3da43aed7577500
Mike Danielson <mikedan@tampabay.rr.com>
2016-06-18 15:39:25 -0400

    Add files via upload
    update

--- server.R
+++ server.R
@@ -1,5 +1,4 @@
 library(shiny)
-<<<<<<< HEAD
 library(rvest)
 library(XML)
 ##
@@ -37,31 +36,6 @@ shinyServer(function(input, output) {
                 if (!is.null(input$Name) & !is.null(input$Type)) {
                         if (input$Name != "All") {data <- data[data$Name == input$Name,]}
                         if (input$Type != "All") {data <- data[data$Type == input$Type,]}}
-=======
-library(ggplot2)
-library(rvest)
-library(DT)
-# code here
-url <- "http://www.pjm.com/pub/account/lmpgen/lmppost.html"
-LMP <- html_table(url %>% read_html() %>% html_nodes(xpath='/html/body/center[4]/table'))
-LMP <- as.data.frame(LMP)
-names(LMP)<-c(LMP[1,])
-LMP <- LMP[-c(1),]
-row.names(LMP) <- 1:nrow(LMP)
-updatetime <- url %>% read_html() %>% html_nodes(xpath='/html/body/center[2]') %>% html_text()
-LMP <- LMP[c(1,2,4,3)]
-colnames(LMP)[3] <- paste("HE",substr(updatetime,18,19),sep="","_AVE")
-colnames(LMP)[4] <- paste(substr(updatetime,18,19),":",substr(updatetime,21,22),sep="")
-LMP[,3]<-as.numeric(LMP[,3])
-LMP[,4]<-as.numeric(LMP[,4])
-LMP$DIFF<-LMP[,4]-LMP[,3]
-
-shinyServer(function(input, output) {
-        output$table <- DT::renderDataTable(DT::datatable({
-                data <- LMP
-                if (input$Name != "All") {data <- data[data$Name == input$Name,]}
-                if (input$Type != "All") {data <- data[data$Type == input$Type,]}
->>>>>>> 5259d71b00263863cf0d8b540346a88335fdc389
                 data
         }))
 })
--- ui.R
+++ ui.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 shinyUI(
         fluidPage(
                 includeHTML("documentation.html"),
@@ -6,38 +5,9 @@ shinyUI(
                         h4(strong("Filter by Node")),
                         uiOutput("nameUI"),
                         uiOutput("typeUI")
-                        ),
+                ),
                 mainPanel(
                         fluidRow(DT::dataTableOutput("table"))
                 )
-=======
-library(shiny)
-library(ggplot2)
-library(rvest)
-library(DT)
-# Define the UI with this code
-url <- "http://www.pjm.com/pub/account/lmpgen/lmppost.html"
-LMP <- html_table(url %>% read_html() %>% html_nodes(xpath='/html/body/center[4]/table'))
-LMP <- as.data.frame(LMP)
-names(LMP)<-c(LMP[1,])
-LMP <- LMP[-c(1),]
-row.names(LMP) <- 1:nrow(LMP)
-updatetime <- url %>% read_html() %>% html_nodes(xpath='/html/body/center[2]') %>% html_text()
-LMP <- LMP[c(1,2,4,3)]
-colnames(LMP)[3] <- paste("HE",substr(updatetime,18,19),sep="","_AVE")
-colnames(LMP)[4] <- paste(substr(updatetime,18,19),":",substr(updatetime,21,22),sep="")
-LMP[,3]<-as.numeric(LMP[,3])
-LMP[,4]<-as.numeric(LMP[,4])
-LMP$DIFF<-LMP[,4]-LMP[,3]
-
-shinyUI(
-        fluidPage(
-                includeHTML("documentation.html"),
-                fluidRow(
-                        column(4, selectInput(inputId="Name", label = "Name:", choices = c("All", unique(as.character(LMP$Name))))),
-                        column(4, selectInput(inputId="Type", label = "Type:", choices = c("All", unique(as.character(LMP$Type)))))
-                ),
-                fluidRow(DT::dataTableOutput("table"))
->>>>>>> 5259d71b00263863cf0d8b540346a88335fdc389
         )
 )
